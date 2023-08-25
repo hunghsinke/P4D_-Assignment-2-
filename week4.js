e24f7d21f93890df6410ab13a793f535b8a49c6b
Hung Hsin (Kenny)Ke <78602893+hunghsinke@users.noreply.github.com>
2023-08-25 15:10:19 +1000

    initialize

--- week4.js
+++ week4.js
@@ -1,3 +1,41 @@
 /*
     Paste the code for your week 4 exercise below.
 */
+
+// function setup() {
+//     drawingContext.shadowOffsetX = 5;
+//     drawingContext.shadowOffsetY = -5;
+//     drawingContext.shadowBlur = 10;
+//     drawingContext.shadowColor = "black";
+//     background(200);
+//     ellipse(width/2, height/2, 50, 50);
+//   }
+
+  function setup() {
+    createCanvas(720, 400);
+  }
+
+  function draw() {
+    background(200);
+    ellipse(width/2, height/2, 50, 50);
+    describe('A white circle with black outline in the middle of a gray canvas.');  
+
+    triangle(30, 75, 58, 20, 86, 75);
+    describe('A white triangle with a black outline on a gray canvas.');
+
+    //two circles 
+    noStroke();
+    fill(0, 50);
+    circle(160, 160, 50);
+
+    stroke(0);
+    fill(255);
+    circle(150, 150, 50);
+
+    textFont("helvetica");
+    textSize(36);
+    text('x:' + mouseX, mouseX, mouseY);
+    text('y:' + mouseY, mouseX, mouseY+36);
+
+  }
+
